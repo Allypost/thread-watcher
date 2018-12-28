eedd5a326057454d4c585954d854152cd75cb5ea
Josip Igrec <me@allypost.net>
2018-12-28 23:56:10 +0100

    Centralized fetcher functions into the ThreadWatcher module

--- lib/thread_watcher.ex
+++ lib/thread_watcher.ex
@@ -6,4 +6,14 @@ defmodule ThreadWatcher do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  alias ThreadWatcher.Fetchers
+
+  @spec fetch() :: {:error, binary()} | {:ok, [map()]}
+  def fetch(), do: Fetchers.Boards.fetch()
+
+  @spec fetch(binary()) :: {:error, binary()} | {:ok, [map()]}
+  def fetch(board), do: Fetchers.Board.fetch(board)
+
+  @spec fetch(binary(), number()) :: {:error, binary()} | {:ok, [map()]}
+  def fetch(board, thread), do: Fetchers.Thread.fetch(board, thread)
 end
