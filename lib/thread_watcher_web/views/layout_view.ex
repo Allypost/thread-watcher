1babbc7cb3cd337b21421821dee0d1f3e0873789
Josip Igrec <me@allypost.net>
2019-02-26 06:31:33 +0100

    Global JS config is now managed from the layout_view via the global_js_data function

--- lib/thread_watcher_web/views/layout_view.ex
+++ lib/thread_watcher_web/views/layout_view.ex
@@ -1,3 +1,12 @@
 defmodule ThreadWatcherWeb.LayoutView do
   use ThreadWatcherWeb, :view
+
+  def global_js_data(conn) do
+    %{
+      config: %{
+        api_url: Application.get_env(:thread_watcher, :api_domain, "/api"),
+        api_urls: ThreadWatcher.Handlers.ApiInfo.add_info(:info, conn)
+      }
+    }
+  end
 end
